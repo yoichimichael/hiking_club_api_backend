1d7d7c97399276df2fa74528023dff8f645289d7
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-14 20:44:21 -0700

    working on controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/hikes', to: 'hikes#index'
+
+  get '/members', to: 'members#index'
+
+  get '/participations', to: 'participations#index'
+
+  get '/comments', to: 'comments#index'
+
 end
