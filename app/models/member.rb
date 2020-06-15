438b88e7169fbdb9771ecf7900cc2d024acaaade
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-15 11:12:19 -0700

    host association

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,6 @@ class Member < ApplicationRecord
   has_many :hikes, through: :participations
   has_many :comments
   has_many :hosted_hikes, foreign_key: 'host_id', class_name: 'Hike'
+  
   # validates :username, presence: true 
 end
