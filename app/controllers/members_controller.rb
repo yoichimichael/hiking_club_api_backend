089439ceb62d78558df0c86ed73d4434a0b59ec4
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-06-14 21:10:53 -0700

    added routes and controller actions for members and hikes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,4 +5,32 @@ class MembersController < ApplicationController
     render json: members, except: [:created_at, :updated_at]
   end
 
+  def show
+    member = Member.find(params[:id])
+    render json: member, except: [:created_at, :updated_at]
+  end
+
+  def create
+    member = Member.new(member_params)
+    member.save
+    render json: member, except: [:created_at, :updated_at]
+  end
+
+  def update
+    member = Member.find(params[:id])
+    member.update(post_params)
+    render json: member, except: [:created_at, :updated_at]
+  end
+
+  def delete
+    member = Member.find(params[:id])
+    member.destroy
+  end
+
+  private
+ 
+  def member_params
+    params.require(:member).permit(:name, :username, :email, :img_url)
+  end
+
 end
